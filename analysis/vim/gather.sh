a13a5d4f028fe21aa99d00f71c99ca5944bcc3c3
Joseph Wecker <joseph.wecker@gmail.com>
2011-09-09 11:52:47 -0700

    Remove windows line endings

--- analysis/vim/gather.sh
+++ analysis/vim/gather.sh
@@ -4,7 +4,7 @@ set -x
 export LC_ALL=C
 locate -i '*.vim' | xargs egrep -sIhi '^[ 	]*hi' | tr -u '\011' ' ' \
 	| grep -v '<' | egrep -v '[ ]clear([ ]|$)' \
-	| sed -E -e 's/[ ]*".*//g' -e 's/^[ ]*//g' -e 's/[ ]+/ /g' \
+	| sed -E -e 's/[ ]*".*//g' -e 's/^[ ]*//g' -e 's/[ ]+/ /g' -e 's///g' \
 	         -e 's/^[hH][iI]([gG][hH][lL][iI][gG][hH][tT])?([ ]*[dD][eE][fF]([aA][uU][lL][tT])?)?/hi/g' \
 			 -e 's/^hi!?[ ]*[lL][iI][nN][kK]/map/g' -e 's/^hi([^ ].*|$)//g' \
 			 -e 's/^hi[ ][^ ]+$//g' -e 's/^hi/def/g' \
