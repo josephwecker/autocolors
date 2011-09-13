f3e19132e0426aeddadfc4b425f73afff93d66e8
Joseph Wecker <jwecker@justin.tv>
2011-09-13 15:47:20 -0700

    Got actual vim file generation working more or less (for gui values at least). Coming along...

--- lib/autocolors/colorscheme.rb
+++ lib/autocolors/colorscheme.rb
@@ -1,16 +1,19 @@
 module AutoColors
   class ColorScheme
     require 'webster'
-    attr_accessor :name, :seed, :contrast, :saturation
+    require 'autocolors/color'
+    attr_accessor :name, :seed, :contrast, :saturation, :dark, :light
     def initialize(name=nil)
       srand rand(0xffffffff)
       name ||= Webster.new.random_word
       @name = name
       @seed = @name.hash
       srand @seed
+      @dark = {}
+      @light = {}
       generate
     end
-    
+
     def generate
       bc = nrand(-1,2,-4,5)
       sat = rand * 2.0
@@ -30,13 +33,21 @@ module AutoColors
       @mapping.entries.each do |name, entry|
         [:fg_idx, :bg_idx].each{|k| concrete_index(entry,k)}
         [:fg_intensity, :fg_saturation, :bg_intensity, :bg_saturation].each{|k| concrete_lvl(entry,k)}
+        concrete_style(entry)
       end
 
-      @mapping.entries.to_a.sort_by{|n,e| n}.each do |name, entry|
-        puts "#{name}: #{entry.data[:fg_idx]}|#{entry.data[:fg_intensity]}|#{entry.data[:fg_saturation]}  /  #{entry.data[:bg_idx]}|#{entry.data[:bg_intensity]}|#{entry.data[:bg_saturation]}"
+      @mapping.entries.each do |name, entry|
+        ldat = entry.data.dup
+        ddat = entry.data.dup
+        fg_a, fg_b, _ = @base_colors[ldat[:fg_idx]]
+        bg_a, bg_b, _ = @base_colors[ldat[:bg_idx]]
+        ldat[:fg] = lab(light_i(ldat[:fg_intensity]), ldat[:fg_saturation], fg_a, fg_b)
+        ldat[:bg] = lab(light_i(ldat[:bg_intensity]), ldat[:bg_saturation], bg_a, bg_b)
+        ddat[:fg] = lab( dark_i(ldat[:fg_intensity]), ldat[:fg_saturation], fg_a, fg_b)
+        ddat[:bg] = lab( dark_i(ldat[:bg_intensity]), ldat[:bg_saturation], bg_a, bg_b)
+        @dark[name] = ddat
+        @light[name] = ldat
       end
-      require 'pp'
-      pp @base_colors.each_with_index.map{|d,i| [i,d]}
     end
 
     def concrete_index(entry, k)
@@ -58,9 +69,7 @@ module AutoColors
           if primes == 0 # Directly inherit
             entry.data[k] = entry.parent.data[k]
           else # Modify a bit
-            #puts "--- here --- #{entry.parent.data[k]}, #{primes}, #{entry.depth}" if entry.name == 'lit.string.quoted.backtick'
             entry.data[k] = new_color(entry.parent.data[k], primes, entry.depth)
-            #puts "--- became --- #{entry.data[k]}" if entry.name == 'lit.string.quoted.backtick'
           end
         end
       end
@@ -73,7 +82,7 @@ module AutoColors
       c_minus  = entry.data[k].count('-')
       c_neut   = entry.data[k].count('~')
       if c_parent == 0
-        entry.data[k] = 5 - c_minus + c_plus
+        entry.data[k] = 3 - c_minus + c_plus
       else
         concrete_lvl(entry.parent, k)
         offset = entry.parent.data[k]
@@ -81,11 +90,22 @@ module AutoColors
       end
     end
 
+    def concrete_style(entry)
+      entry.data[:styles] = 'NONE'
+    end
+
+    def dark_i(idx) idx end
+    def light_i(idx) 7 - idx end
+  
+    def lab(intensity, saturation, a, b)
+      Color.new([@intensity[intensity], a * @fcolor[saturation], b * @fcolor[saturation]])
+    end
+
     def new_color(base_idx, diff_level, depth)
       a,b,count = @base_colors[base_idx]
-      base_diff = (diff_level + 1) * 10 / (depth + 1) * count
-      a_dir = rand(2) == 1 ? -1 : 1
-      b_dir = rand(2) == 1 ? -1 : 1
+      base_diff = (diff_level.to_f + 1.0) * 10.0 / ((depth.to_f + 1.0) / 2.0) * count.to_f
+      a_dir = rand(2) == 1 ? -1.0 : 1.0
+      b_dir = rand(2) == 1 ? -1.0 : 1.0
       a_p = a + (base_diff * a_dir)
       b_p = b + (base_diff * b_dir)
       new_idx = @base_colors.size
