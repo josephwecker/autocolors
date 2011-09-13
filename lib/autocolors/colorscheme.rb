eaf80f92d63a70847daa337c385b6292b361d0a2
Joseph Wecker <jwecker@justin.tv>
2011-09-13 13:44:41 -0700

    Concrete color indexes, intensities, and saturation values

--- lib/autocolors/colorscheme.rb
+++ lib/autocolors/colorscheme.rb
@@ -20,7 +20,7 @@ module AutoColors
       @intensity = [00-(bc*3), 20-(bc*2), 45-bc, 50, 60, 65+bc, 90+(bc*2), 110+(bc*3)]
       @fcolor = [0.0, 0.1*sat, 0.2*sat, 0.4*sat, 0.8*sat, 1.6*sat, 3.2*sat, 6.4*sat]
 
-      @base_colors = (1..10).map{|i| [nrand(0.0, 100.0, -120.0, 120.0), nrand(0.0,100.0,-120.0,120.0), 0]}
+      @base_colors = (1..10).map{|i| [nrand(0.0, 100.0, -120.0, 120.0), nrand(0.0,100.0,-120.0,120.0), 1]}
       do_concrete_mapping
     end
 
@@ -29,10 +29,14 @@ module AutoColors
       @mapping = MAPPING.dup
       @mapping.entries.each do |name, entry|
         [:fg_idx, :bg_idx].each{|k| concrete_index(entry,k)}
-        puts "#{name}: #{entry.data[:fg_idx]} / #{entry.data[:bg_idx]}"
+        [:fg_intensity, :fg_saturation, :bg_intensity, :bg_saturation].each{|k| concrete_lvl(entry,k)}
+      end
+
+      @mapping.entries.to_a.sort_by{|n,e| n}.each do |name, entry|
+        puts "#{name}: #{entry.data[:fg_idx]}|#{entry.data[:fg_intensity]}|#{entry.data[:fg_saturation]}  /  #{entry.data[:bg_idx]}|#{entry.data[:bg_intensity]}|#{entry.data[:bg_saturation]}"
       end
       require 'pp'
-      pp @base_colors
+      pp @base_colors.each_with_index.map{|d,i| [i,d]}
     end
 
     def concrete_index(entry, k)
@@ -54,11 +58,26 @@ module AutoColors
           if primes == 0 # Directly inherit
             entry.data[k] = entry.parent.data[k]
           else # Modify a bit
-            require 'pp'
-            pp entry.parent.data[k]
+            #puts "--- here --- #{entry.parent.data[k]}, #{primes}, #{entry.depth}" if entry.name == 'lit.string.quoted.backtick'
             entry.data[k] = new_color(entry.parent.data[k], primes, entry.depth)
+            #puts "--- became --- #{entry.data[k]}" if entry.name == 'lit.string.quoted.backtick'
           end
         end
+      end
+    end
+
+    def concrete_lvl(entry, k)
+      return if entry.data[k].is_a? Integer
+      c_parent = entry.data[k].count('<')
+      c_plus   = entry.data[k].count('+')
+      c_minus  = entry.data[k].count('-')
+      c_neut   = entry.data[k].count('~')
+      if c_parent == 0
+        entry.data[k] = 5 - c_minus + c_plus
+      else
+        concrete_lvl(entry.parent, k)
+        offset = entry.parent.data[k]
+        entry.data[k] = offset + c_plus - c_minus
       end
     end
 
@@ -70,7 +89,7 @@ module AutoColors
       a_p = a + (base_diff * a_dir)
       b_p = b + (base_diff * b_dir)
       new_idx = @base_colors.size
-      @base_colors[new_idx] = [a_p, b_p, 0]
+      @base_colors[new_idx] = [a_p, b_p, 1]
       @base_colors[base_idx][2] += 1
       return new_idx
     end
