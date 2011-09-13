4637aacef2ec343911d15049e5c9659bc92950a8
Joseph Wecker <jwecker@justin.tv>
2011-09-13 15:46:33 -0700

    Quick interface for generating vim syntax file (and testing)

--- lib/autocolors.rb
+++ lib/autocolors.rb
@@ -1,8 +1,20 @@
 module AutoColors
   require 'autocolors/mapping'
   require 'autocolors/colorscheme'
+  require 'erb'
   VERSION = File.exist?(File.join(File.dirname(__FILE__),'VERSION')) ? File.read(File.join(File.dirname(__FILE__),'VERSION')) : ""
+  TEMPLATE_PATH = File.join(File.dirname(__FILE__),'autocolors','templates')
   class << self
     def version() VERSION end
+
+    def template(name)
+      IO.read(File.join(TEMPLATE_PATH, name))
+    end
+
+    def generate_vim_colors(name=nil)
+      scheme = ColorScheme.new(name)
+      template = ERB.new(template('colorscheme.erb.vim'),nil,'-')
+      return template.result(binding)
+    end
   end
 end
